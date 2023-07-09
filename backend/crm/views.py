662f7d974bee2421769569b84d629738d4ea3070
rg3915 <regis42santos@gmail.com>
2023-07-09 01:54:53 -0300

    Remove todos serializers

--- backend/crm/views.py
+++
@@ -1,9 +0,0 @@
-from crm.models import Customer
-from crm.serializers import CustomerSerializer
-from rest_framework import mixins, permissions, viewsets
-from rest_framework.response import Response
-
-
-class CustomerViewSet(viewsets.ModelViewSet):
-    queryset = Customer.objects.all()
-    serializer_class = CustomerSerializer
