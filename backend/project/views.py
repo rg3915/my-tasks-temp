e4e3f21587ced476bf3d06f4a7164b26d07bc68e
rg3915 <regis42santos@gmail.com>
2023-07-13 02:16:37 -0300

    Retorna os itens em project_list.html

--- backend/project/views.py
+++ backend/project/views.py
@@ -4,6 +4,6 @@ from .forms import ProjectForm
 
 
 def project_list(request):
-    template_name = 'crm/project_list.html'
+    template_name = 'project/project_list.html'
     context = {'form': ProjectForm}
     return render(request, template_name, context)
