c6977bb71e9aeefc91d8378f5ed86702eac7f160
rg3915 <regis.santos.100@gmail.com>
2024-01-06 07:39:09 -0300

    Reset migrations.

--- backend/project/admin.py
+++ backend/project/admin.py
@@ -1,10 +1,16 @@
 from django.contrib import admin
 
-from .models import Project
+from .models import Owner, Project
+
+
+@admin.register(Owner)
+class OwnerAdmin(admin.ModelAdmin):
+    list_display = ('__str__',)
+    search_fields = ('name',)
 
 
 @admin.register(Project)
 class ProjectAdmin(admin.ModelAdmin):
     list_display = ('__str__', 'customer', 'repository_name')
     search_fields = ('title', 'customer__name', 'repository_name')
-    list_filter = ('active', 'customer')
+    list_filter = ('active', 'customer', 'repository_owner')
